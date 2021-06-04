e99517c6b7fd520f2f91f70b28db0776f60c21e9
Mand0019 <84968267+maddy-andrews@users.noreply.github.com>
2021-06-04 14:43:20 +1000

    upload website v1

---
+++ GEOGRAPHICSPROJECT/assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    console.log("Hello world");
+  
+    $(".row").click(function () {
+      $(this).find(".content").slideToggle("fast");
+    });
+  });
+  $(document).ready(function () {
+    console.log("Hello world");
+  
+    $(".row2").click(function () {
+      $(this).find(".content2").slideToggle("fast");
+    });
+  });
+  $(document).ready(function () {
+    console.log("Hello world");
+  
+    $(".row2").click(function () {
+      $(this).find(".vid").slideToggle("fast");
+    });
+  });
+  $(document).ready(function () {
+    console.log("Hello world");
+  
+    $(".row3").click(function () {
+      $(this).find(".content3").slideToggle("fast");
+    });
+  });
+
+  (function () {
+    var inner = ('#inner');
+    inner.on('click', function (e) {
+        inner
+            .hide()
+            .off();
+    });
+});
